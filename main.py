8f63c646509f6c5ed00c86dce7d5303fca00d1f9
Steven Schenk <Steven Schenk>
2016-09-23 13:30:39 +0200

    Sample graph complete. Parsing XML and publish graph

--- main.py
+++ main.py
@@ -1,23 +1,51 @@
+import json
+
 import xmltodict
 from bottle import route, run, template, static_file
 
-list = []
+nodes = []
+edges = []
 
+counter = 500;
 def parse():
-    global  list
+    global  counter
     with open('resources/actions.xml') as fd:
         doc = xmltodict.parse(fd.read())
 
-        for action in doc["actions"]["action"][0]:
-            print(action)
-            list.append(action)
+        for action in doc["actions"]["action"]:
+            dict = {}
+            dictData = {}
+            dictPosition = {}
+            dictData["id"] = action["actionId"]
+            dictData["idInt"] = int(action["actionId"])
+            dictData["name"] = action["actionname"]
+            dictPosition["x"] = 5
+            dictPosition["y"] = 5
+            # dict["classes"] =
+            dict["data"] = dictData
+            dict["position"] = dictPosition
+            nodes.append(dict)
+
+
+            for neighbour in action['neighbours']["neighbour"]:
+                edge = {}
+                edgeData = {}
+                edgeData["id"] = str(counter)
+                edgeData["source"] = dictData["id"]
+                edgeData["target"] = neighbour
+                edgeData["weight"] = 0.5
+                edge["data"] = edgeData
+                nodes.append(edge)
+                counter += 1
 
 
 parse()
 
+
 @route('/')
 def index():
-    global  list
-    return template('index', res=list)
+    global nodes
+    return template('index', res=json.dumps(nodes))
+
 
 run(host='localhost', port=8080)
